2d58520f08f5406b5e531dc9ee70bfb53deb8c76
Max Blancarte <blancarte@gmail.com>
2022-09-14 16:27:03 -0700

    initial commit

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import {Text} from 'react-native';
+import React from 'react';
+// import {NavigationContainer} from '@react-navigation/native';
+import Navigation from './src/navigation/Navigation';
+
+const App = () => {
+  return <Navigation />;
+};
+
+export default App;
